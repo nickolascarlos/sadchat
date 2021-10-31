593062117bc6b9930a2df3b8610f4bece4165a0b
Nickolas Carlos <nickolascarlos@gmail.com>
2021-10-31 20:46:33 -0300

    Alterada a forma de leitura do input

--- keyboard.py
+++ keyboard.py
@@ -1,44 +1,38 @@
-from pynput import keyboard
-from pynput.keyboard import Key
+from datetime import datetime
+import threading
 
 from commands import *
 import buffer
+import state
+import gui
+from communication import send_message
 
-def on_press(key):
-    try:
-        buffer.buffer += key.char
-    except AttributeError as e:
-        if key == Key.space:
-            buffer.buffer += " "
-        elif key == Key.backspace:
-            buffer.buffer = buffer.buffer[:-1] # Apaga
-        elif key == Key.enter:
-            # Verifica se o que está no buffer se trata de um comando
-            # Se for, o processa
-            # Do contrário, assume-se ser uma mensagem
-            if buffer.buffer.startswith("!"):
-                process_command(buffer.buffer)
-            else:
-                # envia_mensagem(buffer.buffer)
-                pass
-            # E então limpa o buffer
-            buffer.clear_buffer()
+def watch_keys():
+    while True:
+        try:
+            p_key_pressed = gui.stdscr.get_wch()
+            key_pressed = ord(p_key_pressed)
+            if key_pressed == 10: # Enter
+                # Verifica se o que está no buffer se trata de um comando
+                # Se for, o processa
+                # Do contrário, assume-se ser uma mensagem
+                if  buffer.get_buffer().startswith("!"):
+                    process_command(buffer.get_buffer())
+                else:
+                    send_message(buffer.get_buffer())
+                # E então limpa o buffer
+                buffer.clear_buffer()
+            elif key_pressed == 127: # Backspace
+                buffer.backspace()
+            elif (32 <= key_pressed <= 126 or 192 <= key_pressed <= 255): # Caractere normal
+                buffer.append(chr(key_pressed))
+        except Exception:
+            pass
+        # # if key_pressed == curses.KEY_ENTER:
+        # state.add_message("quiiboah", str(key_pressed))
 
-def on_release(key):
-    buffer.process_buffer() # 'Corrige' o conteúdo do buffer (acentuação)
-    # if (key == Key.shift):
-    #     print(buffer.buffer)
-    ###
 
 def start_keyboard_listening():
-    # Registrando e iniciando o Listener para o teclado
-    with keyboard.Listener(
-            on_press=on_press,
-            on_release=on_release) as listener:
-        listener.join()
-
-    listener = keyboard.Listener(
-        on_press=on_press,
-        on_release=on_release)
-
-    listener.start()
+    y = threading.Thread(target=watch_keys)
+    y.name = "Keyboard-THREAD"
+    y.start()
