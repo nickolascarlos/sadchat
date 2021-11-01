0fee18b6a0e706761d9a312cc64fe3b1a505a689
Nickolas Carlos <nickolascarlos@gmail.com>
2021-10-31 21:03:04 -0300

    Adicionado arquivo strings.py

---
+++ strings.py
@@ -0,0 +1,17 @@
+no_command = "No command!"
+secret_set = "Secret key set!"
+your_secret_is = "Your secret key is \"%s\""
+your_username_is = "Your username is %s"
+please_set_your_username = "Please, set your username using !setuser {username}"
+username_set = "Your username is now %s"
+insufficient_arguments = "Insufficient arguments for this command"
+forbidden_username = "Please, use another username"
+connected_to = "Connected to %s"
+connecting_to = "Connecting to %s:%s" # IP:PORT
+waiting_for_connection = "Waiting for connection on %s:%s"
+same_usernames_not_allowed = "CONNECTION REJECTED: It's not allowed to use the same usernames"
+lost_connection = "Lost connection"
+friend_disconnected = "%s went offine"
+
+def __(s):
+    return "\n\n%s\n" % (s)
